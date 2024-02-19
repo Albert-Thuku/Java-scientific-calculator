fe23e1863d0dc78aa0740531785d5b6b8302d61e
Esther Jesicah <142402943+EJWanjiku@users.noreply.github.com>
2024-02-19 20:19:59 +0300

    Second Change
    We have added new files that contain the gui

--- ScientificCalculator/src/ScientificCalculator.java
+++ ScientificCalculator/src/ScientificCalculator.java
@@ -1,5 +1,4 @@
 import java.lang.Math;
-import java.util.Scanner;
 
 public class ScientificCalculator {
     private double currentValue;
@@ -65,65 +64,4 @@ public class ScientificCalculator {
     }
 
     // Add more scientific functions as needed
-}
-
-public class main {
-    public static void main(String[] args) {
-        ScientificCalculator calculator = new ScientificCalculator();
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to the Scientific Calculator App!");
-
-        while (true) {
-            System.out.println("Current Value: " + calculator.getCurrentValue());
-            System.out.println("Enter an operator (+, -, *, /, sqrt, sin, cos, tan, ^, q to quit):");
-
-            String operator = scanner.next();
-
-            if (operator.equals("q")) {
-                System.out.println("Thank you for using the Scientific Calculator App!");
-                break;
-            }
-
-            switch (operator) {
-                case "+":
-                    System.out.println("Enter a number:");
-                    calculator.add(scanner.nextDouble());
-                    break;
-                case "-":
-                    System.out.println("Enter a number:");
-                    calculator.subtract(scanner.nextDouble());
-                    break;
-                case "*":
-                    System.out.println("Enter a number:");
-                    calculator.multiply(scanner.nextDouble());
-                    break;
-                case "/":
-                    System.out.println("Enter a number:");
-                    calculator.divide(scanner.nextDouble());
-                    break;
-                case "sqrt":
-                    calculator.squareRoot();
-                    break;
-                case "^":
-                    System.out.println("Enter the exponent:");
-                    calculator.exponent(scanner.nextDouble());
-                    break;
-                case "sin":
-                    calculator.sine();
-                    break;
-                case "cos":
-                    calculator.cosine();
-                    break;
-                case "tan":
-                    calculator.tangent();
-                    break;
-                default:
-                    System.out.println("Invalid operator");
-                    break;
-            }
-        }
-
-        scanner.close();
-    }
 }
